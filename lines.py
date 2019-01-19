fdf9cd33e81f6314f4eb455100a18a15739ae647
kaifergerstrom <kaifergerstrom@gmail.com>
2019-01-18 22:00:58 -0500

    Data is read now

--- lines.py
+++ lines.py
@@ -10,16 +10,19 @@ cap.set(cv2.CAP_PROP_AUTOFOCUS, 0) # Turn the camera autofocus off
 
 WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Set the width and height from camera object
 HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-SENSITIVITY = 50  # Define the hsv sensitivity range for color filtering
+SENSITIVITY = 25  # Define the hsv sensitivity range for color filtering
+ANGLE_OFFSET = 5  # Define the offset range from 90 degrees to consider a "lockon"
+LOCATION_OFFSET = 60  # The amount of pixel offset from the center point in the camera to verify if object is in front
+CONTOUR_LIMIT = 500  # The limit for the size of the contour
 
 
-def slope_angle(p1, p2):  # Basic function to calculate the angle of two points
+def find_angle(p1, p2):  # Basic function to calculate the angle of two points
 	x = p2[0] - p1[0]  # Find the x coordinate
 	y = p2[1] - p1[1]  # Find the y coordinate
 	try:
 		angle = math.degrees(math.atan(y/x))  # Inverse tangent (opposite over adjacent)
 	except ZeroDivisionError:  # If angle is 90 degrees inverse tangent is undefined
-		angle = "at 90 degrees"
+		angle = 90.0
 	return angle  # Return the angle measure
 
 
@@ -44,6 +47,39 @@ def order_points(box):  # Recieves dimensions of bounding box, and sorts through
 	return [points[0][0], points[1][0]]  # Return a multi dimensional array with the two points
 
 
+def verify_angle(theta):  # Check if the angle given is within the range allowed to succesfully drive straight return a boolean value
+	angle_dict = {}  # Create a empty dictionary to organize data
+	if abs(theta) >= 90-ANGLE_OFFSET and abs(theta) <= 90:  # If the angle is within the positive range, and if its negative absolute value
+		value = True
+	else:  # If not in range return False
+		value = False
+
+	angle_dict['angle'] = theta  # Assign the values from the test to keys in dictionary
+	angle_dict['range'] = ANGLE_OFFSET
+	angle_dict['value'] = value
+	return angle_dict  # Return the dictionary
+
+
+def verify_location(p1, p2):  # Check if the location of the midpoints are within the center of the screen to drive straight
+	location_dict = {}
+	p1_offset = int(WIDTH/2) - p1[0]
+	p2_offset = int(WIDTH/2) - p2[0]
+
+	if p1[0] > int((WIDTH/2) - LOCATION_OFFSET) and p1[0] < int((WIDTH/2) + LOCATION_OFFSET):  # If the first midpoint is in the range we good
+		if p2[0] > int((WIDTH/2) - LOCATION_OFFSET) and p2[0] < int((WIDTH/2) + LOCATION_OFFSET):  # If the second midpoint is in the range we good
+			value = True
+		else:   # If not in range return False
+			value = False
+	else:
+		value = False
+
+	location_dict['p1_offset'] = p1_offset  # Assign the values from the test to keys in dictionary
+	location_dict['p2_offset'] = p2_offset
+	location_dict['range'] = LOCATION_OFFSET
+	location_dict['value'] = value
+	return location_dict  # Return the dictionary
+
+
 while True:  # Frame capture loop
 	_, frame = cap.read()  # Capture the frames from the camera object
 
@@ -60,24 +96,35 @@ while True:  # Frame capture loop
 	contours, _ = cv2.findContours(blurred, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)  # Find all the contours in the mask
 
 	if contours:  # Only run if there are contours on the screen
+
 		c = max(contours, key=cv2.contourArea)  # Isolate the largest contour
 
-		box = cv2.minAreaRect(c)  # Find the box dimensions of contour
-		box = cv2.boxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)  # Adjust for rotation
-		box = np.array(box, dtype="int")  # Create a numpy array of box dimensions
+		if c.size > CONTOUR_LIMIT:
 
-		rect = order_points(box)  # Return the two smallest point combinations from box dimensions
-		side1 = rect[0]  # Split the multidimensional array returned from "order_points" into two variables
-		side2 = rect[1]
-		midpoint1 = midpoint(side1[0], side1[1])  # Find the midpoint of each point combinations
-		midpoint2 = midpoint(side2[0], side2[1])
+			box = cv2.minAreaRect(c)  # Find the box dimensions of contour
+			box = cv2.boxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)  # Adjust for rotation
+			box = np.array(box, dtype="int")  # Create a numpy array of box dimensions
 
-		angle = slope_angle(midpoint1, midpoint2)  # Get the angle of the line made by the two points
-		print(angle)  # Just print out the angle for fun
-		
-		cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)  # Draw rest of contour for fun
-		cv2.line(frame, midpoint1, midpoint2, (255,0,0), 2)  # Draw line create by two for fun
-		cv2.line(frame, (int(WIDTH/2), 0), (int(WIDTH/2), HEIGHT), (0,0,255), 2)  # Draw line through screen by two for fun
+			rect = order_points(box)  # Return the two smallest point combinations from box dimensions
+			side1 = rect[0]  # Split the multidimensional array returned from "order_points" into two variables
+			side2 = rect[1]
+			midpoint1 = midpoint(side1[0], side1[1])  # Find the midpoint of each point combinations
+			midpoint2 = midpoint(side2[0], side2[1])
+
+			angle = find_angle(midpoint1, midpoint2)  # Get the angle of the line made by the two points
+
+			'''
+			print(angle)
+			print(int(angle))
+			print(verify_angle(angle))
+			print(verify_location(midpoint1, midpoint2))
+			'''
+			
+			cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)  # Draw rest of contour for fun
+			cv2.line(frame, midpoint1, midpoint2, (255,0,0), 2)  # Draw line create by two for fun
+			cv2.line(frame, (int(WIDTH/2), 0), (int(WIDTH/2), HEIGHT), (0,0,255), 2)  # Draw line through screen by two for fun
+			cv2.line(frame, (int(WIDTH/2) - LOCATION_OFFSET, 0), (int(WIDTH/2) - LOCATION_OFFSET, HEIGHT), (255,0,0), 2)  # Draw line through screen by two for fun
+			cv2.line(frame, (int(WIDTH/2) + LOCATION_OFFSET, 0), (int(WIDTH/2) + LOCATION_OFFSET, HEIGHT), (255,0,0), 2)  # Draw line through screen by two for fun
 
 	cv2.imshow('frame', frame)  # Display the frames
 	cv2.imshow('mask', mask)  # Display the mask
